8a884ea6098bf365f4c36fb05a25f2ae4b80e6d2
Leonardo Narciso <LNarcisoOV@gmail.com>
2019-10-01 14:30:55 -0300

    Adding example for predicate in chapter 4.

--- src/main/java/com/lambdastudy/chapter/Chapter4.java
+++ src/main/java/com/lambdastudy/chapter/Chapter4.java
@@ -3,6 +3,7 @@ package com.lambdastudy.chapter;
 import java.util.List;
 import java.util.function.Consumer;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 import com.lambdastudy.model.User;
 
@@ -11,6 +12,7 @@ class Chapter4 {
 		example1();
 		example2();
 		example3();
+		example4();
 	}
 
 	private static void example1() {
@@ -34,9 +36,19 @@ class Chapter4 {
 	private static void example3() {
 		List<User> usersList = User.createUsersList();
 		
-		usersList.removeIf(u -> u.getScore() > 140 );
-		
-		System.out.println("Users with score smaller than 140:");
+		Predicate<User> predicate = u -> u.getScore() > 140;
+		usersList.removeIf(predicate);
 		usersList.forEach(u -> System.out.println(u.getName()));
 	}
+	
+	private static void example4() {
+		List<User> usersList = User.createUsersList();
+		
+		usersList = usersList.stream().filter(u -> !u.isModerator()).collect(Collectors.toList());
+		
+		System.out.println("Users that aren't moderator:");
+		usersList.forEach(System.out::println);
+	}
+	
+	
 }
