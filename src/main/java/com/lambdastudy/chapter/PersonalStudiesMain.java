e45909e9e2b0f5697eed5e005d3a5d0204158f13
Leonardo Narciso <LNarcisoOV@gmail.com>
2022-10-20 14:23:35 +0000

    Refactor and add limit() example.

--- src/main/java/com/lambdastudy/chapter/PersonalStudiesMain.java
+++ src/main/java/com/lambdastudy/chapter/PersonalStudiesMain.java
@@ -6,18 +6,55 @@ import java.util.List;
 public class PersonalStudiesMain {
 
 	private static final int SKIP_ITEMS_VALUE = 2;
+	private static final int LIMIT_ITEMS_VALUE = 4;
+	private static final List<Integer> INT_LIST = Arrays.asList(1,2,3,4,5,6,7,8,9);
+	
 	public static void main(String[] args) {
-		List<Integer> intList = Arrays.asList(1,2,3,4,5,6,7,8,9);
 		
-		System.out.println("PERSONAL LAMBDA STUDIES");
+		
+		headerText();
 		
 		System.out.println();
 		System.out.println("Full int list: ");
-		intList.stream().skip(SKIP_ITEMS_VALUE).forEach(System.out::println);
+		INT_LIST.stream().forEach(System.out::print);
 		
+		skipExample();
+		limitExample();
+		
+	}
+	
+	private static void skipExample() {
+		spaceBreaker();
+		System.out.println("SKIP()");
 		System.out.println();
 		System.out.println("Int list using skip() method to skip the first " + SKIP_ITEMS_VALUE + " of the list:");
-		intList.stream().skip(SKIP_ITEMS_VALUE).forEach(System.out::println);
+		INT_LIST.stream().skip(SKIP_ITEMS_VALUE).forEach(System.out::print);
+	}
+	
+	private static void limitExample() {
+		spaceBreaker();
+		System.out.println("LIMIT()");
+		System.out.println();
+		System.out.println("Int list using limit() method to get the first " + LIMIT_ITEMS_VALUE + " of the list:");
+		INT_LIST.stream().limit(LIMIT_ITEMS_VALUE).forEach(System.out::print);
+	}
+	
+	private static void headerText() {
+		System.out.println("PERSONAL LAMBDA STUDIES");
+		System.out.println();
+		System.out.println();
+		System.out.println("---------------------------------");
+		System.out.println("INTERMEDIARY OPERATIONS.");
+		System.out.println("---------------------------------");
+		System.out.println();
+		System.out.println();
+		System.out.println("---------------------------------");
+	}
+	
+	private static void spaceBreaker() {
+		System.out.println();
+		System.out.println();
+		System.out.println("---------------------------------");
 	}
 
 }
