62626072a0d2327f7435fc3ec090b7c0b5a12013
Leonardo Narciso <LNarcisoOV@gmail.com>
2023-09-06 21:28:12 +0100

    Refactor.

--- src/main/java/com/completablefuture/CompletableFutureMain.java
+++ src/main/java/com/completablefuture/CompletableFutureMain.java
@@ -4,44 +4,21 @@ import java.util.concurrent.ExecutionException;
 
 public class CompletableFutureMain {
     public static void main(String[] args) {
-        System.out.println("---------------- Processing Synchronous ----------------");
         
         Store worten = new Store();
         Store amazon = new Store();
         Store leroyMerlin = new Store();
         Store ericeira = new Store();    
+        
+        executeSynchronous(worten, amazon, leroyMerlin, ericeira);
+        executeAsynchronous(worten, amazon, leroyMerlin, ericeira);
+        executeAsynchronousWithException(worten, amazon, leroyMerlin, ericeira);
+    }
 
-        long start = System.currentTimeMillis();
-        
-        worten.getPrice();
-        amazon.getPrice();
-        leroyMerlin.getPrice();
-        ericeira.getPrice();
-        
-        System.out.println("Processing time synchronous: " + (System.currentTimeMillis() - start));
-        
-        
-        System.out.println();
-        System.out.println("---------------- Processing Asynchronous ----------------");
-        
-        
-        start = System.currentTimeMillis();
-        
-        worten.getPriceAsync();
-        amazon.getPriceAsync();
-        leroyMerlin.getPriceAsync();
-        ericeira.getPriceAsync();
-        
-        System.out.println("Do something value: "+ doSomething());
-        
-        System.out.println("Processing time Asynchronous: " + (System.currentTimeMillis() - start));
-        
-        
-        System.out.println();
+    private static void executeAsynchronousWithException(Store worten, Store amazon, Store leroyMerlin, Store ericeira) {
         System.out.println("---------------- Processing Asynchronous With Exception----------------");
-        
-        
-        start = System.currentTimeMillis();
+                
+        long start = System.currentTimeMillis();
         
         try {
             worten.getPriceAsyncWithException().get();
@@ -55,6 +32,40 @@ public class CompletableFutureMain {
         System.out.println("Do something value: "+ doSomething());
         
         System.out.println("Processing time Asynchronous: " + (System.currentTimeMillis() - start));
+    }
+
+    private static void executeAsynchronous(Store worten, Store amazon, Store leroyMerlin, Store ericeira) {
+        System.out.println("---------------- Processing Asynchronous ----------------");
+        
+        long start = System.currentTimeMillis();
+        
+        worten.getPriceAsync();
+        amazon.getPriceAsync();
+        leroyMerlin.getPriceAsync();
+        ericeira.getPriceAsync();
+        
+        System.out.println("Do something value: "+ doSomething());
+        
+        System.out.println("Processing time Asynchronous: " + (System.currentTimeMillis() - start));
+        
+        
+        System.out.println();
+    }
+
+    private static void executeSynchronous(Store worten, Store amazon, Store leroyMerlin, Store ericeira) {
+        System.out.println("---------------- Processing Synchronous ----------------"); 
+
+        long start = System.currentTimeMillis();
+        
+        worten.getPrice();
+        amazon.getPrice();
+        leroyMerlin.getPrice();
+        ericeira.getPrice();
+        
+        System.out.println("Processing time synchronous: " + (System.currentTimeMillis() - start));
+        
+        
+        System.out.println();
     }
     
     private static int doSomething() {
